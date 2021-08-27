0a9ad7de2dec1565d43bf0f41dd6bf45a8c2a083
drinatj <71970164+drinatj@users.noreply.github.com>
2021-08-27 17:15:17 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = process.env.REACT_APP_CLIENT_ID;
-export const redirectUri = "https://generasi-gigih-homework-final.vercel.app";
+export const redirectUri = "https://generasi-gigih-homework-final-9wffwdysp-final-project.vercel.app/";
 export const scopes = "playlist-modify-private";
